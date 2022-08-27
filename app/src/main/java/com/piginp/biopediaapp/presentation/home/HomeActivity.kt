ff2b4765acdcbff372df82be01ea7c3161a1bdb2
pigrist <pavel.pigin@gmail.com>
2022-08-27 20:03:19 +0300

    0.1.1 alpha - Flashlight button

--- app/src/main/java/com/piginp/biopediaapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/piginp/biopediaapp/presentation/home/HomeActivity.kt
@@ -7,16 +7,26 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.budiyev.android.codescanner.*
 import com.piginp.biopediaapp.R
+import com.piginp.biopediaapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var scannerView: CodeScannerView
     private lateinit var codeScanner: CodeScanner
+
+    private var flashLightStatus: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init() // Тут даются значения переменным
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
             PackageManager.PERMISSION_DENIED
@@ -27,15 +37,31 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun startScanning() {
-        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+    private fun init() {
+        scannerView = findViewById(R.id.scanner_view)
         codeScanner = CodeScanner(this, scannerView)
+
+        // TODO: Тут хорошо бы сделать смену цвета кнопки
+        binding.flashButton.setOnClickListener {
+            if (!flashLightStatus) {
+                codeScanner.isFlashEnabled = true
+                flashLightStatus = true
+                binding.flashlightOnTv.isVisible = true
+            } else {
+                codeScanner.isFlashEnabled = false
+                flashLightStatus = false
+                binding.flashlightOnTv.isVisible = false
+            }
+        }
+    }
+
+    private fun startScanning() {
 
         // Parameters (default values)
         codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
         codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
         // ex. listOf(BarcodeFormat.QR_CODE)
-        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE// or CONTINUOUS
         codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
         codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
         codeScanner.isFlashEnabled = false // Whether to enable flash or not
@@ -56,10 +82,6 @@ class HomeActivity : AppCompatActivity() {
                 codeScanner.startPreview()
             }
         }
-
-//        scannerView.setOnClickListener {
-//            codeScanner.startPreview()
-//        }
     }
 
     override fun onResume() {
